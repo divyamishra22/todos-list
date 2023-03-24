97b0f129c669ee7795e4c2af88cecd562df2d1e1
Divya Mishra <mishradivyagkp1999@gmailcom>
2023-03-24 11:49:46 +0530

    added Todos and TodoItem

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 
 import './App.css';
 import Header from "./My Components/Header";
+import { TodoItem } from './My Components/TodoItem';
+import { Todos } from './My Components/Todos';
 
 function App() {
 
   return (
     <>
     <Header/>
+    <Todos/>
+    <TodoItem/>
     </>
   );
 }
