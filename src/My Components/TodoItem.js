a58561eabb0264c99661e6bc8aecafc50cda3662
Divya Mishra <mishradivyagkp1999@gmailcom>
2023-03-24 13:13:38 +0530

    text-center

--- src/My Components/TodoItem.js
+++ src/My Components/TodoItem.js
@@ -5,7 +5,7 @@ export const TodoItem = ({todoitem}) => {
     <div>
         <h4>{todoitem.title}</h4>
         <p>{todoitem.desc}</p>
-        <button className="btn btn-danger">Delete</button>
+        <button className="btn btn-danger btn-sm">Delete</button>
         </div>
   )
 }
--- src/My Components/Todos.js
+++ src/My Components/Todos.js
@@ -3,8 +3,8 @@ import { TodoItem } from './TodoItem'
 
 export const Todos = ({todos}) => {              //todos to get todos at index =0.
   return (
-    <div>
-        <h3>LIST</h3>
+    <div className='text-center'>
+        <h3 className="text-center">LIST</h3>
         <TodoItem todoitem={todos[0]}/>           
     </div>
   )
